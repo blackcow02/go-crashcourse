f6b15f7c02f58183738cec3819096d41aa60bbf3
John Nguyen <jtnguyen236@gmail.com>
2016-03-04 09:48:58 +0800

    initial commit

---
+++ concurrency/main.go
@@ -0,0 +1,54 @@
+package concurrency
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+func main() {
+	flag.Parse()
+
+	sites := flag.Args()
+	start := time.Now()
+
+	WithWG(sites)
+	// WithoutWG(sites)
+	fmt.Printf("Entire process took %s\n", time.Since(start))
+}
+
+// WithWG runs the fetcher with wait groups
+func WithWG(sites []string) {
+	var wg sync.WaitGroup
+	wg.Add(len(sites))
+
+	for _, site := range sites {
+		go func(site string) {
+			defer wg.Done()
+			begin := time.Now()
+			if _, err := http.Get(site); err != nil {
+				fmt.Println(site, err)
+				return
+			}
+			fmt.Printf("Site %q took %s to retrieve.\n", site, time.Since(begin))
+		}(site)
+
+	}
+	wg.Wait()
+}
+
+// WithoutWG runs the fetcher without wait groups or concurrency
+func WithoutWG(sites []string) {
+	for _, site := range sites {
+		fmt.Println(site)
+		begin := time.Now()
+		if _, err := http.Get(site); err != nil {
+			fmt.Println(site, err)
+			return
+		}
+		fmt.Printf("Site %q took %s to retrieve.\n", site, time.Since(begin))
+	}
+
+}
